2791b0e80e595bd8d3d071e22e053c55236eeaf0
Christian Mugione <chmugione@gmail.com>
2024-01-24 22:56:39 -0300

    .

---
+++ app/Http/Controllers/GrupoController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GrupoController extends Controller
+{
+    public function index()
+    {
+        //por convención se le llama index a la funcion que muestra pagina principal
+    }
+
+    public function create()
+    {
+        //por convención se le llama create cuando es para crear
+    }
+
+    public function show()
+    {
+        //por convención se le llama show cuando es para mostrar
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,41 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return 'Welcome to this website made with Laravel';
 });
+
+//Ruta simple
+Route::get('/ruta', function () {
+    return 'Welcome to page Ruta';
+});
+
+//Ruta 
+Route::get('/ruta/loquesea', function () {
+    return "Ruta Loquesea";
+});
+
+//Ruta con variable
+Route::get('/ruta/{variable}', function ($variable) {
+    return "La variable es: $variable";
+});
+
+//Ruta con mas variables
+Route::get('/ruta/{variable}/{otravariable}', function ($variable, $otravariable) {
+    return "La variable es: $variable y la otra es: $otravariable";
+});
+
+//Variable opcional (hay que darle un valor por defecto)
+Route::get('/otraruta/{variable}/{otravariable?}', function ($variable, $otravariable = "Xdefecto") {
+    return "La variable es: $variable y la otra es: $otravariable";
+});
+
+//CONTROLADORES
+//Para crear un controlador se va a la terminal y se pone:
+//  > php artisan make:controller NombreController
+// por convencion el nombre del controlador debe terminar en Controller
+//Ruta que usa controlador
+Route::get('home/', HomeController::class);
